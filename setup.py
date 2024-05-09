8876c5e8f1089674a4f4ddaad7f51ef312d0d133
alcamilo1207 <81839026+alcamilo1207@users.noreply.github.com>
2024-05-09 01:46:56 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pv-milp',
+      version='1.0',
+      description='milp solver for IMECE paper',
+      license='BSD',
+      author='Camilo Lopez',
+      author_email='alcamilopez@gmail.com',
+      url='https://github.com/alcamilo1207/pv-milp',
+      packages=['ideal'],
+)
